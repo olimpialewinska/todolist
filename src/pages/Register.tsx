7b2ee7be9e3d98f9c04765f614db0e7bb538e74d
Olimpia <o.lewinska.336@studms.ug.edu.pl>
2023-05-18 19:50:22 +0200

    Initial commit

---
+++ src/pages/Register.tsx
@@ -0,0 +1,8 @@
+export default function RegisterPage() {
+  return (
+    <div>
+      <h1>Register</h1>
+      <p>This is the register page.</p>
+    </div>
+  );
+}
---
+++ src/pages/Task.tsx
@@ -0,0 +1,8 @@
+export default function TaskPage() {
+  return (
+    <div>
+      <h1>Taks</h1>
+      <p>This is the task page.</p>
+    </div>
+  );
+}
